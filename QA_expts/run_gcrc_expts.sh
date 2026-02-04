fca1d611d736aaf27220d9a605caceecf4e1f4b4
Drew Prinster <46874730+drewprinster@users.noreply.github.com>
2026-02-04 13:37:44 -0500

    Cleaned up QA experiments code

---
+++ QA_expts/run_gcrc_expts.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#SBATCH -t 24:00:00 
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --partition=parallel
+#SBATCH --gpus-per-task=0
+source .venv/bin/activate
+python run_gcrc_expts.py --method_names monotized_losses_crc ltt gcrc --score_names logprobs # logprobs frequency selfeval
